3131779c1aeebcb2722d0c2888ece17261a382b7
snugglebear <vikramh50@aim.com>
2017-04-06 07:50:11 -0500

    Initial commit

--- src/main/java/com/sweng/customerqueue/AppConfig.java
+++ src/main/java/com/sweng/customerqueue/AppConfig.java
@@ -8,4 +8,5 @@ import org.springframework.boot.SpringApplication;
 public class AppConfig {
     public static void main(String[] args) {
         SpringApplication.run(AppConfig.class, args);
+    }
 }
