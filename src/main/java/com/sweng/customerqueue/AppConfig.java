bc368fe485fca35791cf6fbb58f4c066adddfa17
snugglebear <vikramh50@aim.com>
2017-04-06 07:44:10 -0500

    Initial commit

---
+++ src/main/java/com/sweng/customerqueue/AppConfig.java
@@ -0,0 +1,11 @@
+package com.sweng.customerqueue;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by vikramh on 4/6/17.
+ */
+public class AppConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+}
